6c99cb8fa7449d02bfb9e55fd528a206cc602e43
iankurao <kuraoian@gmail.com>
2019-05-27 09:49:52 +0300

    initialize extension

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
+
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    
+
+    
