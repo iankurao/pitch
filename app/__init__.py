14d2bbafa2ac6b423b1ddad841692cbd020fad2c
iankurao <kuraoian@gmail.com>
2019-05-27 11:59:55 +0300

    register blueprint instance in our create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,10 @@ def create_app(config_name):
     app = Flask(__name__)
 
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+
   #  Creating the app configurations
     app.config.from_object(config_options[config_name])
 
