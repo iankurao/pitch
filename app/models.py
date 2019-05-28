ffddfd961b81cce6105da06523997bb3f79aa994
iankurao <kuraoian@gmail.com>
2019-05-28 11:13:57 +0300

    configure models

--- app/models.py
+++ app/models.py
@@ -1,11 +1,25 @@
-from . import db   
+from . import db  
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(255))
 
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_hash = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_hash,password)
 
     def __repr__(self):
         return f'User {self.username}'
