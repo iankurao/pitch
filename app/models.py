5a9c3397eb2a5f00a7eb1565b36bd876863ee59f
iankurao <kuraoian@gmail.com>
2019-05-29 10:03:40 +0300

    add pitch and model classes and establish a database connection

--- app/models.py
+++ app/models.py
@@ -1,13 +1,28 @@
 from . import db  
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    password_hash = db.Column(db.String(255))
+    about=db.Column(db.String(500))
+    profile=db.Column(db.String(250))
+    comments=db.relationship('Comments',backref='user',lazy="dynamic")
+    pass_secure = db.Column(db.String(255))
+   
 
     @property
     def password(self):
@@ -15,14 +30,55 @@ class User(UserMixin,db.Model):
 
     @password.setter
     def password(self, password):
-        self.pass_hash = generate_password_hash(password)
+        self.pass_secure = generate_password_hash(password)
 
 
     def verify_password(self,password):
-        return check_password_hash(self.pass_hash,password)
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username}'
+
+'''
+Pitch ideas table
+'''
+
+class Pitches(db.Model):
+    __tablename__="pitches"
+    id=db.Column(db.Integer,primary_key=True)
+    pitch=db.Column(db.String(500))
+    title=db.Column(db.String(250))
+    author=db.Column(db.String(250))
+    categ=db.Column(db.String(250))
+    role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+    def __repr__(self):
+        return f'User {self.pitch}'
+
+'''
+Comments model
+'''
+#establishing a database connection
+class Comments(db.Model):
+    __tablename__='comments'
+    id=db.Column(db.Integer,primary_key=True)
+    pitch_id=db.Column(db.Integer)
+    pitch_title=db.Column(db.String)
+    comments=db.Column(db.String)
+    posted=db.Column(db.DateTime,default=datetime.utcnow)
+    user_id=db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,id):
+        comments=Comments.query.filter_by(pitch_id=id).all()
+        return comments
+
+
+
+
 
 
 class Role(db.Model):
@@ -33,3 +89,5 @@ class Role(db.Model):
     users = db.relationship('User',backref = 'role',lazy="dynamic")
     def __repr__(self):
         return f'User {self.name}'
+
+      
